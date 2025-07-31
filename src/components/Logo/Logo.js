1c0d4c34cecff0890e3dca87a1f1f763bd41eaf9
Egor Dorosh <egor.dorosh@check24.de>
2025-07-31 07:57:52 +0200

    Fix logo linter error

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 
 export default function Logo({style}) {
+  // Error: Image with src "/logo.svg" is missing required "width" property.
   return (
     <Image
       src="/logo.svg"
       alt="Logo"
       style={style}
+      width={160}
+      height={47.16}
     />
   );
 }
