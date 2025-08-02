fa77a471de700419e02a8d8aae5199544d4e6204
egordorosh <egordororsh@gmail.com>
2025-08-02 20:15:13 +0200

    Revert "Refactor Datenschutz and Impressum pages: replace static text with template literals for improved readability and consistency."
    This reverts commit d8f270de64d5eb8642e6a23caf76c2d74ce00586.

--- src/app/impressum/page.js
+++ src/app/impressum/page.js
@@ -32,53 +32,53 @@ export default function ImpressumPage() {
           Angaben gemäß § 5 TMG
         </Typography>
         <Typography variant="body1" sx={bodyTextStyle}>
-          {'MOOD BEAUTY'}<br />
-          {'Natalia Dorosh'}<br />
-          {'Otl-Aicher-Str. 46'}<br />
-          {'80807 München'}<br />
-          {'Deutschland'}
+          MOOD BEAUTY<br />
+          Natalia Dorosh<br />
+          Otl-Aicher-Str. 46<br />
+          80807 München<br />
+          Deutschland
         </Typography>
 
         <Typography variant="h2" sx={sectionHeadingStyle}>
           Kontakt
         </Typography>
         <Typography variant="body1" sx={bodyTextStyle}>
-          {'Telefon: 0152 073 89 443'}<br />
-          {'E-Mail: moodbeauty.de@gmail.com'}
+          Telefon: 0152 073 89 443<br />
+          E-Mail: moodbeauty.de@gmail.com
         </Typography>
 
         <Typography variant="h2" sx={sectionHeadingStyle}>
           Inhaltlich Verantwortlicher gemäß § 55 Abs. 2 RStV
         </Typography>
         <Typography variant="body1" sx={bodyTextStyle}>
-          {'Natalia Dorosh'}<br />
-          {'Otl-Aicher-Str. 46, 80807 München'}<br />
-          {'Deutschland'}
+          Natalia Dorosh<br />
+          Otl-Aicher-Str. 46, 80807 München<br />
+          Deutschland
         </Typography>
 
         <Typography variant="h2" sx={sectionHeadingStyle}>
           Umsatzsteuer-Identifikationsnummer
         </Typography>
         <Typography variant="body1" sx={bodyTextStyle}>
-          {'Umsatzsteuer-Identifikationsnummer gemäß § 27a UStG: '}<strong>DE14420320346</strong>
+          Umsatzsteuer-Identifikationsnummer gemäß § 27a UStG: <strong>DE14420320346</strong>
         </Typography>
 
         <Typography variant="h2" sx={sectionHeadingStyle}>
           Berufsbezeichnung und berufsrechtliche Regelungen
         </Typography>
         <Typography variant="body1" sx={bodyTextStyle}>
-          {'Berufsbezeichnung: Permanent Make-Up Artist'}<br />
-          {'Verliehen in: Deutschland'}<br />
-          {'Kammerzugehörigkeit: nicht anwendbar'}<br />
-          {'Berufsrechtliche Regelungen: Tätigkeit im Rahmen der Handwerksordnung'}
+          Berufsbezeichnung: Permanent Make-Up Artist<br />
+          Verliehen in: Deutschland<br />
+          Kammerzugehörigkeit: nicht anwendbar<br />
+          Berufsrechtliche Regelungen: Tätigkeit im Rahmen der Handwerksordnung
         </Typography>
 
         <Typography variant="h2" sx={sectionHeadingStyle}>
           EU-Streitschlichtung
         </Typography>
         <Typography variant="body1" sx={bodyTextStyle}>
-          {'Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: https://ec.europa.eu/consumers/odr/.'}<br />
-          {'Unsere E-Mail-Adresse finden Sie oben im Impressum.'}
+          Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: https://ec.europa.eu/consumers/odr/.<br />
+          Unsere E-Mail-Adresse finden Sie oben im Impressum.
         </Typography>
 
         <Typography variant="h2" sx={sectionHeadingStyle}>
